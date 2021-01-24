9b06ebe2e2f29d7994d348f9be23bbf9de5247bb
Bradley Donahue <donahue.bradley@gmail.com>
2021-01-23 23:48:39 -0500

    Implement burger specific db queries

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+const orm = require('../config/orm');
+
+async function createBurger(burgerName) {
+  const newBurger = {
+    burger_name: burgerName,
+    devoured: false,
+  };
+
+  const result = await orm.insertOne('burgers', newBurger);
+  return result;
+}
+
+async function updateBurger(id) {
+  const result = await orm.updateOne('burgers', { devoured: true }, id);
+  return result;
+}
+
+async function viewBurgers() {
+  const burgers = await orm.selectAll('burgers');
+  return burgers;
+}
+
+module.exports = {
+  createBurger,
+  updateBurger,
+  viewBurgers,
+};
